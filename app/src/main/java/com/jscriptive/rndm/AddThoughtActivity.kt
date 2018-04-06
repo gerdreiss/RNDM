6879c4035eeab6365b50711c6943e67931b65d31
gerdreiss <gerdreiss@jscriptive.com>
2018-04-06 22:14:34 +0200

    Add Thought to Firebase Firestore

--- app/src/main/java/com/jscriptive/rndm/AddThoughtActivity.kt
+++ app/src/main/java/com/jscriptive/rndm/AddThoughtActivity.kt
@@ -1,10 +1,17 @@
 package com.jscriptive.rndm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_add_thought.*
+import java.util.*
 
 class AddThoughtActivity : AppCompatActivity() {
+
+    var selectedCategory: String = FUNNY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,19 +19,53 @@ class AddThoughtActivity : AppCompatActivity() {
     }
 
     fun addFunnyClicked(view: View) {
-
+        if (selectedCategory == FUNNY) {
+            addFunnyBtn.isChecked = true
+        } else {
+            addSeriousBtn.isChecked = false
+            addCrazyBtn.isChecked = false
+            selectedCategory = FUNNY
+        }
     }
 
     fun addSeriousClicked(view: View) {
-
+        if (selectedCategory == SERIOUS) {
+            addSeriousBtn.isChecked = true
+        } else {
+            addFunnyBtn.isChecked = false
+            addCrazyBtn.isChecked = false
+            selectedCategory = SERIOUS
+        }
     }
 
     fun addCrazyClicked(view: View) {
-
+        if (selectedCategory == CRAZY) {
+            addCrazyBtn.isChecked = true
+        } else {
+            addFunnyBtn.isChecked = false
+            addSeriousBtn.isChecked = false
+            selectedCategory = CRAZY
+        }
     }
 
     fun addPostClicked(view: View) {
+        val data = mapOf(
+                Pair("category", selectedCategory),
+                Pair("numComments", 0),
+                Pair("numLikes", 0),
+                Pair("thought", addThoughtTxt.text.toString()),
+                Pair("timestamp", FieldValue.serverTimestamp()),
+                Pair("username", addUsernameTxt.text.toString())
+        )
 
+        FirebaseFirestore.getInstance().collection(THOUGHTS_REF)
+                .add(data)
+                .addOnSuccessListener {
+                    finish()
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("Exception", "Could not add post: $exception")
+                }
     }
 
 }
---
+++ app/src/main/java/com/jscriptive/rndm/Constants.kt
@@ -0,0 +1,6 @@
+package com.jscriptive.rndm
+
+const val FUNNY = "funny"
+const val SERIOUS = "serious"
+const val CRAZY = "crazy"
+const val THOUGHTS_REF = "thoughts"
