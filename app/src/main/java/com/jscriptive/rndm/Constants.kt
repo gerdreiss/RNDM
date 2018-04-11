5dcc2cba1a3c6120517ef58845972f3f3ec0e0d6
gerdreiss <gerdreiss@jscriptive.com>
2018-04-11 21:15:45 +0200

    create user account

--- app/src/main/java/com/jscriptive/rndm/Constants.kt
+++ app/src/main/java/com/jscriptive/rndm/Constants.kt
@@ -6,6 +6,7 @@ const val CRAZY = "crazy"
 const val POPULAR = "popular"
 
 const val THOUGHTS_REF = "thoughts"
+const val USERS_REF = "users"
 
 // Fields
 const val CATEGORY = "category"
@@ -14,3 +15,4 @@ const val NUM_LIKES = "numLikes"
 const val THOUGHT_TXT = "thought"
 const val TIMESTAMP = "timestamp"
 const val USERNAME = "username"
+const val DATE_CREATED = "dateCreated"
--- app/src/main/java/com/jscriptive/rndm/CreateUserActivity.kt
+++ app/src/main/java/com/jscriptive/rndm/CreateUserActivity.kt
@@ -1,21 +1,61 @@
 package com.jscriptive.rndm
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_create_user.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+
+        auth = FirebaseAuth.getInstance()
     }
 
     fun createCreateAcctClicked(view: View) {
+        val email = createEmailTxt.text.toString()
+        val pwd = createPwdTxt.text.toString()
+        val username = createUsernameTxt.text.toString()
+        // TODO validate input
 
+        auth.createUserWithEmailAndPassword(email, pwd)
+                .addOnSuccessListener { result ->
+                    val changeRequest = UserProfileChangeRequest.Builder()
+                            .setDisplayName(username)
+                            .build()
+                    result.user.updateProfile(changeRequest)
+                            .addOnFailureListener { exception ->
+                                Log.e("ERROR", "Could not update display name: ${exception.localizedMessage}")
+                            }
+
+                    val data = mapOf(
+                            Pair(USERNAME, username),
+                            Pair(DATE_CREATED, FieldValue.serverTimestamp())
+                    )
+                    FirebaseFirestore.getInstance().collection(USERS_REF).document(result.user.uid)
+                            .set(data)
+                            .addOnSuccessListener {
+                                finish()
+                            }
+                            .addOnFailureListener { exception ->
+                                Log.e("ERROR", "Could not add user document: ${exception.localizedMessage}")
+                            }
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("ERROR", "Could not create user: ${exception.localizedMessage}")
+                }
     }
 
     fun createCancelClicked(view: View) {
-
+        finish()
     }
 }
