8ffe723392023071b862a474bf8720ae9c11ccec
gerdreiss <gerdreiss@jscriptive.com>
2018-04-11 20:52:06 +0200

    login and create account activities

---
+++ app/src/main/java/com/jscriptive/rndm/CreateUserActivity.kt
@@ -0,0 +1,21 @@
+package com.jscriptive.rndm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun createCreateAcctClicked(view: View) {
+
+    }
+
+    fun createCancelClicked(view: View) {
+
+    }
+}
